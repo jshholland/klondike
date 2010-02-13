ec3cebe030b1dcc47c57411cc7fd48149b8959b5
Josh Holland <jrh@joshh.co.uk>
2010-02-13 18:18:55 +0000

    Added face_up attribute to Card class
    To allow easier telling whether a card is face up or down, it can now be
    tracked via the face_up attribute on Card instances. Face-down cards
    display in lower-case.
    
    The make_deck function was also streamlined to use a one-line listcomp
    instead of nested for loops.

--- klondike.py
+++ klondike.py
@@ -2,18 +2,20 @@
 # A program to simulate the well-known game of Klondike (or Patience, or
 # Solitaire, depending on your nationality
 
-import random
-
 ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 suits = ("C", "D", "S", "H")
 
 class Card(object):
-    def __init__(self, rank, suit):
+    def __init__(self, rank, suit, face_up=True):
         self.rank = rank
         self.suit = suit
+        self.face_up = face_up
 
     def __repr__(self):
-        return self.rank + self.suit
+        if self.face_up:
+            return self.rank + self.suit
+        else:
+            return (self.rank + self.suit).lower()
 
     rank_trans = dict([(v, i) for (i, v) in enumerate(ranks)])
     def __cmp__(self, other):
@@ -30,10 +32,5 @@ class Card(object):
             return True
         return False
 
-def make_deck():
-    deck = []
-    for rank in ranks:
-        for suit in suits:
-            deck.append(Card(rank, suit))
-    random.shuffle(deck)
-    return deck
+def make_deck(face_up=False):
+    return [Card(rank, suit, face_up) for suit in suits for rank in ranks]
