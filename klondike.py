41f95aab0c3ad937e45a2a69dce12fd3e3b9171d
Josh Holland <jrh@joshh.co.uk>
2010-08-06 17:46:08 +0100

    General tidy-up
    Bit of a tidy up after leaving it for a while. Made Deck a proper class.

--- klondike.py
+++ klondike.py
@@ -2,7 +2,7 @@
 # A program to simulate the well-known game of Klondike (or Patience, or
 # Solitaire, depending on your nationality
 
-import copy
+import random
 
 ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 suits = ("C", "D", "S", "H")
@@ -19,54 +19,52 @@ class Card(object):
         else:
             return (self.rank + self.suit).lower()
 
-    rank_trans = dict([(v, i) for (i, v) in enumerate(ranks)])
+    rank_trans = dict((v, i) for (i, v) in enumerate(ranks))
     def __cmp__(self, other):
-        return cmp(self.__class__.rank_trans[self.rank],
-                   other.__class__.rank_trans[other.rank])
+        return cmp(self.rank_trans[self.rank],
+                   other.rank_trans[other.rank])
 
     def follows(self, other):
         if self < other:
             return False
         if self.suit != other.suit:
             return False
-        if self.__class__.rank_trans[self.rank] - 1 == \
-            other.__class__.rank_trans[other.rank]:
+        if self.rank_trans[self.rank] - 1 == \
+            other.rank_trans[other.rank]:
             return True
         return False
 
-def make_deck(face_up=False):
-    return [Card(rank, suit, face_up) for suit in suits for rank in ranks]
+class Deck(object):
+    def __init__(self, shuffled=True):
+        self.data = [Card(rank, suit) for suit in suits for rank in ranks]
+        if shuffled:
+            self.shuffle()
 
-def turn_up(tableau):
-    for pile in range(7) + ['waste']:
-        try:
-            tableau[pile][-1].face_up = True
-        except IndexError:
-            pass
+    def __repr__(self):
+        return ', '.join(str(card) for card in self.data)
 
-def deal(deck):
-    deck = copy.deepcopy(deck)
-    tableau = {}
-    for suit in suits:
-        tableau[suit] = []
-    for pile_num in range(7):
-        tableau[pile_num] = []
-    tableau['stock'] = []
-    tableau['waste'] = []
+    def __len__(self):
+        return len(self.data)
 
-    start = 0
-    while start < 7:
-        for pile in range(start, 7):
-            tableau[pile].append(deck.pop())
-        start += 1
-    tableau['stock'] = deck
-    turn_up(tableau)
-    return tableau
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        return iter(self.data)
+
+    def __contains__(self, item):
+        return item in self.data
+
+    def deal_card(self):
+        card = self.data.pop()
+        card.face_up = True
+        return card
+
+    def shuffle(self):
+        random.shuffle(self.data)
+
 
 if __name__ == '__main__':
-    from random import shuffle
     from pprint import pprint as pp
-    deck = make_deck()
-    shuffle(deck)
-    tab = deal(deck)
-    pp(tab)
+    deck = Deck()
+    pp(deck)
