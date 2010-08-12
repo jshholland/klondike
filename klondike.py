a7d03fd7bca8fad4a4ea78ad155b8253605640e0
Josh Holland <jrh@joshh.co.uk>
2010-08-12 23:15:39 +0100

    Added docstrings

--- klondike.py
+++ klondike.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
-# A program to simulate the well-known game of Klondike (or Patience, or
-# Solitaire, depending on your nationality)
+"""
+A program to simulate the well-known game of Klondike (or Patience, or
+Solitaire, depending on your nationality)
+"""
 
 import random
 
@@ -8,7 +10,17 @@ ranks = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 suits = ("C", "D", "S", "H")
 
 class Card(object):
+    """
+    Class to represent a single playing card. Keeps track of suit, rank and
+    colour as well as whether it is face-up or not. A face-up card is printed
+    out in capitals, a face down one in lower case.
+    """
+
     def __init__(self, rank, suit, face_up=True):
+        """
+        Initialise a Card with its rank and suit. Works out colour based on
+        alternating colours in the suits list.
+        """
         self.rank = rank
         self.suit = suit
         self.face_up = face_up
@@ -37,6 +49,10 @@ class Card(object):
                    other._rank_trans[other.rank])
 
     def follows(self, other):
+        """
+        Predicate to tell whether self follows other (in terms of foundation
+        piles).
+        """
         if self < other:
             return False
         if self.suit != other.suit:
@@ -47,6 +63,10 @@ class Card(object):
         return False
 
     def can_go_on(self, other):
+        """
+        Predicate to tell whether self can be placed on top of another within
+        the tableau.
+        """
         if self.colour == other.colour:
             return False
         if self._rank_trans[self.rank] + 1 == \
@@ -55,6 +75,13 @@ class Card(object):
         return False
 
 class Deck(object):
+    """
+    Class to represent a deck made out of cards. Builds a deck out of each
+    possible combination of suit and rank face-down. Will shuffle the deck
+    using the random.shuffle function from the Python standard library
+    according to the shuffled argument passed in.
+    """
+
     def __init__(self, shuffled=True):
         self.data = [Card(rank, suit, False) for suit in suits for rank in ranks]
         if shuffled:
@@ -78,15 +105,31 @@ class Deck(object):
         return item in self.data
 
     def deal_card(self, face_up=True):
+        """
+        Remove the last card from the deck and return it (face-up according to
+        the argument passed in.
+        """
         card = self.data.pop()
         card.face_up = face_up
         return card
 
     def shuffle(self):
+        """
+        Shuffle the deck.
+        """
         random.shuffle(self.data)
 
 class Tableau(object):
+    """
+    Represent the tableau in a game of patience. Ideally supposed to be as
+    modifiable as possible, but there is a little bit of hard-coded values in
+    there.
+    """
+
     def __init__(self, deck, num_piles=7):
+        """
+        Initialise the tableau with a given deck and number of piles.
+        """
         self.waste = []
         self.stock = []
         self.avail = []
@@ -114,6 +157,9 @@ class Tableau(object):
     __repr__ = __str__
 
     def turn_up(self):
+        """
+        Turn all the available cards in the tableau face up.
+        """
         for card in self.avail:
             card.face_up = True
 
@@ -121,6 +167,9 @@ class Tableau(object):
             card.face_up = True
 
     def playable(self):
+        """
+        Iterate on all the playable cards in the tableau.
+        """
         for pile in self.pile:
             if pile:
                 yield pile[-1]
@@ -128,6 +177,10 @@ class Tableau(object):
             yield self.avail[-1]
 
     def deal_stock(self, num=3):
+        """
+        Deal off (at most, if the deck is nearly empty) num cards from the
+        stock onto the waste pile.
+        """
         for i in range(num):
             try:
                 card = self.stock.pop()
@@ -137,6 +190,10 @@ class Tableau(object):
                 pass
 
     def replace_stock(self):
+        """
+        Replace the stock by turning the waste pile upside down. Null operation
+        if there are still cards in the stock.
+        """
         if self.stock:
             return
         self.stock = list(reversed(self.avail))
@@ -145,17 +202,27 @@ class Tableau(object):
             card.face_up = False
 
     def _find_and_delete(self, card):
+        """
+        Utility method to find and delete a card playable from the tableau
+        (usually as a precursor to putting a copy somewhere else).
+        """
         for pile in self.pile + [self.avail]:
             if pile and card == pile[-1]:
                 del pile[-1]
                 break
 
     def move_to_foundation(self, card):
+        """
+        Move the given card to the relevant foundation pile.
+        """
         self._find_and_delete(card)
         self.foundation[card.suit].append(card)
         self.turn_up()
 
     def move_onto(self, card, other_card):
+        """
+        Move a single card onto another face-up card in the tableau.
+        """
         # Don't use self._find_and_delete so we can find the one we're
         # moving onto in the same iteration.
         for pile in self.pile + [self.avail]:
@@ -166,12 +233,19 @@ class Tableau(object):
         self.turn_up()
 
     def move_to_empty(self, card):
+        """
+        Move a card onto an empty pile in the tableau.
+        """
         first_empty = filter(lambda l: not l, self.pile)[0]
         self._find_and_delete(card)
         first_empty.append(card)
         self.turn_up()
 
 def solve(tableau, max_goes=5):
+    """
+    Solver function for standard patience. Gives up after max_goes times
+    through the stock without moving anything.
+    """
     goes_since_moving = 0
     def _tidy():
         goes_since_moving = 0
